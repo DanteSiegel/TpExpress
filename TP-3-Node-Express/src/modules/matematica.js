aea3a34704384d5fd92c9ac4fd18e5896529d45f
DANTE SIEGEL <47492509@ort>
2024-04-19 08:12:45 -0300

    papux

--- TP-3-Node-Express/src/modules/matematica.js
+++ TP-3-Node-Express/src/modules/matematica.js
@@ -1,8 +1,11 @@
-/* Este es el módulo "matematicas" */
+
+
 const PI = 3.14;
 
 function sumar(x, y) {
+
     return x + y;
+
 }
 
 const restar = (x, y) => x - y;
@@ -10,7 +13,9 @@ const restar = (x, y) => x - y;
 const multiplicar = (a, b) => a * b;
 
 const dividir = (a, b) => {
+
     if (b === 0) {
+        
         throw new Error("No se puede dividir por cero");
     }
     return a / b;
