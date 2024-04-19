b57e9df601c620c2a91d6e12769dcc846e6f21a4
DANTE SIEGEL <47492509@ort>
2024-04-19 08:15:56 -0300

    dadasiubdai

--- TpExpress/index.js
+++ TpExpress/index.js
@@ -12,63 +12,95 @@ app.get('/', (req, res) => {
 })
 
 app.get('/saludar/:nombre', (req, res) => {
+
     res.status(200).send(`Hola ${req.params.nombre}`);
+
 })
 
 app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
+
     const { ano, mes, dia } = req.params;
+
     const fechaString = `${ano}-${mes}-${dia}`;
+
     const fechaParseada = Date.parse(fechaString);
 
+
     if (!isNaN(fechaParseada)) {
+
         res.status(200).send('Fecha válida');
+
     } else {
+
         res.status(400).send('Fecha inválida');
+
     }
 });
 
 app.get('/matematica/sumar/:n1/:n2', (req, res) => {
+
     const n1 = req.params.numero1;
+
     const n2 = req.params.numero2;
+
     const resultado = sumar(parseFloat(n1), parseFloat(n2));
+
     res.status(200).send(resultado.toString());
 });
 
 app.get('/matematica/restar/:n1/:n2', (req, res) => {
+
     const n1 = parseFloat(req.params.n1);
+
     const n2 = parseFloat(req.params.n2);
+
     const resultado = restar(n1, n2);
+
     res.status(200).send(resultado.toString());
 });
 
 app.get('/matematica/multiplicar/:n1/:n2', (req, res) => {
+
     const n1 = parseFloat(req.params.n1);
+
     const n2 = parseFloat(req.params.n2);
+
     const resultado = multiplicar(n1, n2);
+
     res.status(200).send(resultado.toString());
 });
 
 app.get('/matematica/dividir/:n1/:n2', (req, res) => {
     const n1 = parseFloat(req.params.n1);
+
     const n2 = parseFloat(req.params.n2);
+
     try {
         const resultado = dividir(n1, n2);
+
         res.status(200).send(resultado.toString());
+
     } catch (error) {
+
         res.status(400).send(error.message);
+
     }
 });
 
 app.get('/alumnos', (req, res) => {
+
     res.status(200).send(alumnosArray);
+
 });
 
 app.get('/alumnos/:dni', (req, res) => {
+
     const dni = req.params.dni;
 
     const alumno1 = alumnosArray.find(alumno => alumno.DNI === dni);
     
     if (alumno1) {
+        
         res.status(200).send(alumno1);
 
     } else {
